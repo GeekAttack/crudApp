c0fd1d9c067e7deb5fecf04fd842f228a7a1f7ba
Geekon <nancybogati@gmail.com>
2019-02-10 11:36:48 +0545

    Added Android Crud app made at office with Mohendra

---
+++ MyCrudAndroidNinjaApp/app/src/main/java/com/example/nancybogati/mycrudandroidninjaapp/ObjectStudent.java
@@ -0,0 +1,12 @@
+package com.example.nancybogati.mycrudandroidninjaapp;
+
+public class ObjectStudent {
+
+    int id;
+    String firstname;
+    String email;
+
+    public ObjectStudent(){
+
+    }
+}
